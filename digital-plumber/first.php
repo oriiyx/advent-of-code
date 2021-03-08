fb0627447de514000e0a7ceb550cad63e0712359
Peter Paravinja <paravinja92@gmail.com>
2021-03-08 21:52:26 +0100

    Finished first part of the digital plumber - didnt reformat anything - got correct answer

--- digital-plumber/first.php
+++ digital-plumber/first.php
@@ -1,58 +1,69 @@
 <?php
-$pipes = [
-    0 => [
-        2
-    ],
-    1 => [
-        1
-    ],
-    2 => [
-        0,
-        3,
-        4
-    ],
-    3 => [
-        2,
-        4
-    ],
-    4 => [
-        2,
-        3,
-        6
-    ],
-    5 => [
-        6
-    ],
-    6 => [
-        4,
-        5
-    ]
-];
 
-$mappedPipes = [];
+$pipes = array();
 
-foreach ($pipes as $key => $pipe) {
-    if ($key === 0) {
-        array_push($mappedPipes, 0);
-        foreach ($pipe as $item) {
-            array_push($mappedPipes, $item);
-        }
-        continue;
-    }
-    foreach ($pipe as $item) {
-        if (in_array($key, $mappedPipes)) {
-            foreach ($mappedPipes as $mappedPipe) {
-                array_push($mappedPipes, $item);
-            }
-        }
+$handle = fopen("input.txt", "r");
 
-        foreach ($mappedPipes as $mappedPipe) {
-            if ($item === $mappedPipe) {
-                array_push($mappedPipes, $item);
-            }
+if ($handle) {
+    while (!feof($handle)) {
+        $line = fgets($handle);
+
+        $explodedString = explode(' <-> ', $line);
+        $secondaryIntegers = explode(',', $explodedString[1]);
+
+        foreach ($secondaryIntegers as $integer) {
+            $pipes[intval($explodedString[0])][] = intval($integer);
         }
     }
-
 }
-$mappedPipes = array_unique($mappedPipes);
-var_dump($mappedPipes);
+
+fclose($handle);
+
+$mappedPipes = array();
+
+arrayChecker($mappedPipes, $pipes);
+
+function arrayChecker(array $mappedPipes, array $pipes)
+{
+    $oldArray = $mappedPipes;
+
+    $mappedPipes = getPipes($pipes, $mappedPipes);
+
+    $mappedPipes = array_unique($mappedPipes);
+
+    if (array_values($oldArray) == array_values($mappedPipes)) {
+        var_dump('same');
+        var_dump(count($mappedPipes));
+    } else {
+        unset($oldArray);
+        arrayChecker($mappedPipes, $pipes);
+    }
+}
+
+function getPipes(array $pipes, array $mappedPipes): array
+{
+    foreach ($pipes as $key => $pipe) {
+        if ($key === 0) {
+            array_push($mappedPipes, 0);
+            foreach ($pipe as $item) {
+                array_push($mappedPipes, $item);
+            }
+            $mappedPipes = array_unique($mappedPipes);
+            continue;
+        }
+        foreach ($pipe as $item) {
+            if (in_array($key, $mappedPipes)) {
+                array_push($mappedPipes, $item);
+            }
+
+            foreach ($mappedPipes as $mappedPipe) {
+                if ($item === $mappedPipe) {
+                    array_push($mappedPipes, $item);
+                }
+            }
+            $mappedPipes = array_unique($mappedPipes);
+        }
+    }
+
+    return $mappedPipes;
+}
